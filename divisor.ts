deba45ca9066d5e0aa8440b1026d0eeedce33be5
Nathan <Nathan>
2025-05-31 14:10:04 +0100

    updated folder structure for top75

--- divisor.ts
+++ divisor.ts
@@ -6,24 +6,21 @@ let common = "";
 
 function checkCommonString(str1: string, str2: string): string {
   if (str1 + str2 != str2 + str1) {
-    return ""
-
-  }else{
-    
-    const lenGCD = checkDivides(str1.length, str2.length)
-    return str1.substring(0, lenGCD)
+    return "";
+  } else {
+    const lenGCD = checkDivides(str1.length, str2.length);
+    return str1.substring(0, lenGCD);
   }
   return "";
 }
 
 function checkDivides(a: number, b: number): number {
-
-    return b === 0 ? a : checkDivides(b, a % b)
+  return b === 0 ? a : checkDivides(b, a % b);
 }
 
-
-
-
 let result = "";
-result = checkCommonString("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX");
+result = checkCommonString(
+  "TAUXXTAUXXTAUXXTAUXXTAUXX",
+  "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"
+);
 console.log(result);
--- greatestCandies.ts
+++ greatestCandies.ts
@@ -1,11 +1,13 @@
-const candies = [2, 3, 5, 1, 3]
-const extraCandies = 3
+const candies = [2, 3, 5, 1, 3];
+const extraCandies = 3;
 
-const maxCandies = Math.max(...candies)
-let greatestCandies: boolean[] = []
+const maxCandies = Math.max(...candies);
+let greatestCandies: boolean[] = [];
 
-for(let i = 0; i < candies.length; i++){
-    candies[i] + extraCandies >= maxCandies ? greatestCandies.push(true): greatestCandies.push(false)
+for (let i = 0; i < candies.length; i++) {
+  candies[i] + extraCandies >= maxCandies
+    ? greatestCandies.push(true)
+    : greatestCandies.push(false);
 }
 
-console.log(greatestCandies)
+console.log(greatestCandies);
